f00c4dc1b63054e512f603f9a8bedb7df99aa808
hasan-furkan <81443587+hasan-furkan@users.noreply.github.com>
2022-06-01 17:18:55 +0300

    commit

---
+++ pages/body/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import { useState } from 'react';
+function Header({ title }) {
+  return (
+      <Head>
+          <title>{title ? title : "Title calisiyor"}</title>
+      </Head>
+  );
+}
+
+export default function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() { 
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  );
+}
+
+
